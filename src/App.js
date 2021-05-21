faf8e2ab81f3be7a3af26f86f752ec0b329f4488
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-05-21 12:58:05 -0700

    footer placement complete. all home page content placement complete. now to add functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './pages/Navbar';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import Footer from './pages/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <Contact />
+      <Footer />
     </div>
   );
 }
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import FacebookIcon from '../assets/photos/FacebookIconBlack.png';
+import InstagramIcon from '../assets/photos/InstagramIconBlack.png';
+import TwitterIcon from '../assets/photos/TwitterIconBlack.png';
+import '../styles/footer.css';
 
 const Footer = () => {
-  <div>
-    padding
-    Quote:
-    social media icons 
-    copy right info
-    padding
+  return(
+    <div className="footer-container">
+      {/* padding
+      Quote:
+      social media icons
+      copy right info
+      padding */}
+      <p className="footer-quote">It Will Be Our Honor To Serve You In The Ministry Of FASHION.</p>
+      <p className="footer-number">469.665.9580</p>
+      <div className="social-media-icons-wrapper">
+        <a href='https://www.facebook.com/HarlemHaberdashery/'>
+          <img src={FacebookIcon} className="social-media-icons" />
+        </a>
+        <a href='https://www.instagram.com/haberdasherynyc/'>
+          <img src={InstagramIcon} sty className="social-media-icons"/>
+        </a>
+        <a href='https://twitter.com/haberdasherynyc/'>
+          <img src={TwitterIcon} sty className="social-media-icons"/>
+        </a>
+      </div>
+      <p className="footer-number">©2020 by THE CREATIVE STYLEHAUS FASHION FIRM.</p>
+
+
   </div>
+  )
 }
+
+  // footerLogo: {
+  //   display: 'flex',
+  //   justifyContent: 'center',
+  //   alignSelf: 'center',
+  //   resizeMode: 'cover',
+  //   marginTop: 50,
+  //   marginBottom: 50,
+  //   width: 175,
+  //   height: 175
+  // }
+
 
 export default Footer;
