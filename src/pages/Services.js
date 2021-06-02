76990be4c735d2f3b897b3d28259ab7ef4444c5e
Yohanes Demissie <Yohanesdemissie@gmail.com>
2021-06-02 14:54:29 -0700

    added personal and licensable images to services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,6 +3,8 @@ import Navbar from './Navbar.js';
 import Contact from './Contact.js';
 import Footer from './Footer.js';
 import '../styles/services.css';
+import BellaFour from '../assets/photos/Bella-four.JPG';
+import CSHPortrait from '../assets/photos/CSH-portrait.jpg';
 
 const Services = () => {
   return(
@@ -15,22 +17,26 @@ const Services = () => {
       </div>
       <div className="service-items-container">
         <h1 className="runway-text">RUNWAY PRODUCTION</h1>
-        <img className="runway-image" src="https://imageproxy.themaven.net//https%3A%2F%2Ffashionista.com%2F.image%2FMTczMDEzMDkzMDM3NjQ3MDg0%2Fpyer-moss-spring-2020.png" />
+        <img className="runway-image" src="https://www.essence.com/wp-content/uploads/2020/02/1206170872-scaled.jpg?width=600" />
       </div>
 
       <div className="service-items-container">
         <h1 className="runway-text">CREATIVE DIRECTION & STYLING</h1>
-        <img className="runway-image" src="https://www.isdi.in/shortcourses/wp-content/uploads/2020/06/1-2.jpg" />
+                {/* <img className="runway-image" src="https://www.isdi.in/shortcourses/wp-content/uploads/2020/06/1-2.jpg" /> */}
+
+        <img className="runway-image" src={BellaFour} />
       </div>
 
       <div className="service-items-container">
         <h1 className="runway-text">IMAGE CONSULTING</h1>
-        <img className="runway-image" src="https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2016/02/Shamayim2.jpg" />
+        {/* <img className="runway-image" src="https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2016/02/Shamayim2.jpg" /> */}
+        <img className="runway-image"src={CSHPortrait} />
       </div>
 
       <div className="service-items-container">
         <h1 className="runway-text">EDUCATION</h1>
-        <img className="runway-image" src="https://fashionunited.com/images/202009/fashion_education_1.jpg" />
+        {/* <img className="runway-image" src="https://fashionunited.com/images/202009/fashion_education_1.jpg" /> */}
+        <img className="runway-image" src="https://live.staticflickr.com/4173/34013071573_e0f57994d6_b.jpg" />
       </div>
       <Contact />
       <Footer />
